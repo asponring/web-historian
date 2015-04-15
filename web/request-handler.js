43974e9f1d47b3119575f1b9b8918c91e237374c
asponring <asponring@gmail.com>
2015-04-15 15:05:35 -0700

    A little progress, 404 works

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,31 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var urlParser = require('url');
 // require more modules/folders here!
 
+var routes = {
+  '/': true
+};
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  // res.end(archive.paths.list);
+
+  var parts = urlParser.parse(req.url);
+  var route = routes[parts.pathname];
+  if (route) {
+    sendResponse(res);
+  } else {
+    console.log("404");
+    sendResponse(res, "Not Found", 404);
+  }
+
+  // sendResponse(res);
+
+
+};
+
+var sendResponse = function(response, data, statusCode){
+  statusCode = statusCode || 200;
+  response.writeHead(statusCode);
+  response.end();
 };
